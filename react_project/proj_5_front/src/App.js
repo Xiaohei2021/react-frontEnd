eb4895e93a434d81904778c267c27d1eb9712632
Leon <hualiang.tian@gmail.com>
2022-03-11 14:23:49 -0500

    tinkering with link and path

--- react_project/proj_5_front/src/App.js
+++ react_project/proj_5_front/src/App.js
@@ -1,6 +1,8 @@
-import CookBookIndex from './containers/CookBookIndex';
-import RecipeInput from './components/RecipeInput';
 import { Link, Route, Routes } from 'react-router-dom';
+import CookBookIndex from "./containers/CookBookIndex";
+import RecipeInput from "./components/RecipeInput";
+import Home from './components/Home';
+
 
 function App() {
   return (
@@ -8,15 +10,14 @@ function App() {
       <h1>
         Leon's Cook Book
       </h1>      
-      <Link to="/"> Home </Link><br/>
-      <Link to="/recipes"> Saved Recipes </Link><br/>
-      <Link to="/recipes/new"> Create Own Recipes </Link><br/>
-      <Routes>
-        <Route exact path="/recipes" element={<CookBookIndex/>} /> 
-        <Route exact path="/recipes/new" element={<RecipeInput/>} />  
-      </Routes> 
+        {/* <Link to="/recipes"> Saved Recipes </Link><br/>
+        <Link to="/recipes/new"> Create Own Recipes </Link><br/>
 
-      {/* <CookBookIndex /> */}
+        <Routes>
+            <Route exact path="/recipes" element={< CookBookIndex/>} />
+            <Route exact path="/recipes/new" element={< RecipeInput/>} />  
+        </Routes>  */}
+      <Home/>
     </div>
   );
 }
--- react_project/proj_5_front/src/components/Home.js
+++ react_project/proj_5_front/src/components/Home.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom';
+import CookBookIndex from '../containers/CookBookIndex';
+import RecipeInput from './RecipeInput';
+
 
 export default function Home() {
   return (
     <div>
-        Home
+        <Link to="/"> Home </Link><br/>
+        <Link to="/recipes"> Saved Recipes </Link><br/>
+        <Link to="/recipes/new"> Create Own Recipes </Link><br/>
+
+        <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/recipes" element={< CookBookIndex/>} />
+            <Route exact path="/recipes/new" element={< RecipeInput/>} />  
+        </Routes> 
+
+        {/* <CookBookIndex /> */}
+        
     </div>
   )
 }
