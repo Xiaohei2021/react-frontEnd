cd6732e700428ec488aed539c8ed86d28f4ec40b
Leon <hualiang.tian@gmail.com>
2022-03-13 16:27:11 -0400

    drying out code for partially copying an object

--- react_project/proj_5_front/src/components/Dish.js
+++ react_project/proj_5_front/src/components/Dish.js
@@ -1,21 +1,31 @@
 import React from 'react'
 
 export default function Dish(props) {
-  const {strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, 
-      strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12,
-      strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, 
-      strIngredient19, strIngredient20 } = props.dish
+
+
+  // solution #1
+  // const {strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, 
+  //     strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12,
+  //     strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, 
+  //     strIngredient19, strIngredient20 } = props.dish
   
+ // const ingredient = {strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, 
+  //     strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12,
+  //     strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, 
+  //     strIngredient19, strIngredient20  }
+
+
+  // solution #2
+
+  // const meal = props.dish
    
-
-  const ingredient = {strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, 
-      strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12,
-      strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, 
-      strIngredient19, strIngredient20  }
-
   // Object.keys(obj).
   // filter((key) => key.includes('Name')).
   // reduce((cur, key) => { return Object.assign(cur, { [key]: obj[key] })}, {});
+
+  // Object.keys(meal).
+  // filter((key) => key.includes('strIngredient')).
+  // reduce((cur, key) => { return Object.assign(cur, { [key]: neal[key] })}, {});
 
   debugger
   return (
@@ -25,7 +35,12 @@ export default function Dish(props) {
         {/* {props.dish.strInstructions} */}
         <img src={props.dish.strMealThumb}/>
         {/* {props.dish.strIngredient1} */}
-        {ingredient}
+
+
+
+
+        {/* {ingredient} */}
+        {/* {  Object.keys(meal).filter((key) => key.includes('strIngredient')).reduce((cur, key) => { return Object.assign(cur, { [key]: meal[key] })}, {})} */}
     </div>
   )
 }
