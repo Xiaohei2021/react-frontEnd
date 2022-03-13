388b16fd3607bbc0e584618c7cad828ad9836970
Leon <hualiang.tian@gmail.com>
2022-03-12 20:42:36 -0500

    tinkering with api data

--- react_project/proj_5_front/src/components/Dish.js
+++ react_project/proj_5_front/src/components/Dish.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
 export default function Dish(props) {
-  let {strIngredient1,strIngredient2,strIngredient3,strIngredient4, strIngredient20 } 
-  // const ingredient = {...props.slice(strIngredient1, strIngredient20) }
-  Object.keys(obj).
-  filter((key) => key.includes('Name')).
-  reduce((cur, key) => { return Object.assign(cur, { [key]: obj[key] })}, {});
+  let {strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, 
+      strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12,
+      strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, 
+      strIngredient19, strIngredient20 } = props.dish
+  
+   
 
-  debugger
+  const ingredient = {strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, 
+      strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12,
+      strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, 
+      strIngredient19, strIngredient20  }
+
+  // Object.keys(obj).
+  // filter((key) => key.includes('Name')).
+  // reduce((cur, key) => { return Object.assign(cur, { [key]: obj[key] })}, {});
+
+  // debugger
   return (
     <div>
         Dish
@@ -15,6 +25,7 @@ export default function Dish(props) {
         {/* {props.dish.strInstructions} */}
         <img src={props.dish.strMealThumb}/>
         {/* {props.dish.strIngredient1} */}
+        {ingredient}
     </div>
   )
 }
