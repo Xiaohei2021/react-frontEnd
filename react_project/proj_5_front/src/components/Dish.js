3dd80c76d44491a6e6125d630ec4ce5e777fd767
Leon <hualiang.tian@gmail.com>
2022-03-13 19:14:33 -0400

    extracted object value and assigned to an new array. then filtered array to only keep index with values

--- react_project/proj_5_front/src/components/Dish.js
+++ react_project/proj_5_front/src/components/Dish.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Dish(props) {
 
 
-  // solution #1
+  // solution #1{
   // const {strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, 
   //     strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12,
   //     strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, 
@@ -13,10 +13,10 @@ export default function Dish(props) {
   //     strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12,
   //     strIngredient13, strIngredient14, strIngredient15, strIngredient16, strIngredient17, strIngredient18, 
   //     strIngredient19, strIngredient20  }
+// }
 
 
   // solution #2
-
   // const meal = props.dish
    
   // Object.keys(obj).
@@ -27,7 +27,18 @@ export default function Dish(props) {
   // filter((key) => key.includes('strIngredient')).
   // reduce((cur, key) => { return Object.assign(cur, { [key]: neal[key] })}, {});
 
-  debugger
+
+
+  //solution #3 
+  const meal = props.dish
+  //Object.fromEntries(Object.entries(obj).filter(([key]) => key.includes('Name')));
+  const ingredient = Object.fromEntries(Object.entries(meal).filter(([key]) => key.includes('strIngredient')));
+
+  let data = Object.keys(ingredient).map(function(key){ return(ingredient[key])})
+
+  let filtered = data.filter(i => i.length > 0)
+
+  // debugger
   return (
     <div>
         Dish
@@ -35,12 +46,11 @@ export default function Dish(props) {
         {/* {props.dish.strInstructions} */}
         <img src={props.dish.strMealThumb}/>
         {/* {props.dish.strIngredient1} */}
+{/* 
+        {ingredient} */}
+        {/* {data} */}
+        {filtered}
 
-
-
-
-        {/* {ingredient} */}
-        {/* {  Object.keys(meal).filter((key) => key.includes('strIngredient')).reduce((cur, key) => { return Object.assign(cur, { [key]: meal[key] })}, {})} */}
     </div>
   )
 }
