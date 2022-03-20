00cb7d2a3df3a378e4bdb5fdad3dd2cc997fdeff
Leon <hualiang.tian@gmail.com>
2022-03-19 23:03:10 -0400

    deleting notes componnent, adding edit function will make notes component obselete

--- react_project/proj_5_front/src/containers/SurpriseContainer.js
+++ react_project/proj_5_front/src/containers/SurpriseContainer.js
@@ -1,10 +1,12 @@
 // import React, { Component } from 'react'
 import React from 'react'
 
-import FoodList from '../components/surprises/SurpriseList'
-import { connect } from 'react-redux'
+// import FoodList from '../components/surprises/SurpriseList'
+// import { connect } from 'react-redux'
 import {fetchSurprise } from '../actions/surpriseActions/surprise'
 import { Outlet, Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 
 
 // class SurpriseContainer extends Component {
@@ -48,14 +50,27 @@ import { Outlet, Link } from 'react-router-dom';
 
 export default function SurpriseContainer() {
   const dispatch = useDispatch();
-  const recipeDB = useSelector(state => state.db.ownRecipe)
+  const surpriseDB = useSelector(state => state.db.randomRecipes)
  
 
   useEffect( ()=> {
-    dispatch( fetchSavedRecipe() )}, [])
+    dispatch( fetchSurprise() )}, [])
+
+    
   // debugger
 
   return (
-    <div>SurpriseContainer</div>
+    <div>
+      <nav style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
+          }}>
+            <Link to="/home">Home</Link> |{""}
+            <Link to="new">Create New Recipe</Link> |{""}
+            <Link to="list">See All Recipes</Link> |{""}
+          </nav>
+         {/* { recipeDB.map(r => <li key={r.id} recipe={r}>{r.name}</li>)}  */}
+          <Outlet context={ surpriseDB }/> 
+    </div>
   )
 }
